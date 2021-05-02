9511598cf6f40b5f1f46b85d496d9b0371a038bc
Voltaire405 <jlma.ing@protonmail.com>
2021-05-01 22:34:57 -0500

    surname added in author model and endpoints added

--- routes/authors.js
+++ routes/authors.js
@@ -10,7 +10,65 @@ router.get('/', async (req, res) => {
   try {
     let filters = {};
     if (req.query.pais) filters = { pais: req.query.pais };
+
     const authors = await Author.find(filters);
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * @description Ask for authors that publications is less or equal than 20 and his country is Colombia
+ * @returns Name and surnames for this authors
+ */
+router.get('/query1', async (req, res) => {
+  try {
+    let filters = {};
+    let projections = {};
+
+    filters = { publicados: {$lte: 20}, pais: {$eq: 'Colombia'}};
+    projections = {nombre: 1, apellido: 1, _id: 0};
+
+    const authors = await Author.find(filters, projections);
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * @description Ask for authors that havent surname
+ * @returns name of the authors who meet the condition 
+ */
+ router.get('/query2', async (req, res) => {
+  try {
+    let filters = {};
+    let projections = {};
+
+    filters = { apellido: {$exists:false}};
+    projections = {nombre:1, _id:0};
+
+    const authors = await Author.find(filters, projections);
+    res.json(authors);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+/**
+ * @description Or conditional applied to clause publications great than 20 and country equal to Argentina
+ * @returns surname of the authors who meet the condition 
+ */
+ router.get('/query3', async (req, res) => {
+  try {
+    let filters = {};
+    let projections = {};
+
+    filters = { $or:[{publicados: {$gt: 20}}, {pais: {$eq: 'Argentina'}}]};
+    projections = {apellido:1, _id:0};
+
+    const authors = await Author.find(filters, projections);
     res.json(authors);
   } catch (err) {
     res.status(500).json({ message: err.message });
